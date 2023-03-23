ae44cda50be114aed6d84c67c962a3473642b32f
Antônio Vinícius <antoniovrdacosta@gmail.com>
2023-03-23 20:29:48 -0300

    feat: Add typed theme to app

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,12 +1,8 @@
-import {
-  Container,
-  Title
-} from './styles'
+import * as Styled from './styles'
 
 export function Groups() {
   return (
-    <Container>
-      <Title>Groups</Title>
-    </Container>
+    <Styled.Container>
+    </Styled.Container>
   );
 }
