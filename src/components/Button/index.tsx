d36a223cce9e5299c7ff9ce11c767bd61e996f69
Antônio Vinícius <antoniovrdacosta@gmail.com>
2023-03-23 22:23:47 -0300

    feat: Create reusable button component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,17 @@
+import { TouchableOpacityProps } from 'react-native';
+import * as Styled from './styles'
+
+type Props = TouchableOpacityProps & {
+  title: string;
+  type?: Styled.ButtonTypeStyleProps
+}
+
+export function Button({ title, type = 'PRIMARY', ...rest }: Props) {
+  return (
+    <Styled.Container type={type} {...rest}>
+      <Styled.Title>
+        {title}
+      </Styled.Title>
+    </Styled.Container>
+  )
+}
