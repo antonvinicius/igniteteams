8244e23236e639773793a5204a538accc859a9f2
Antônio Vinícius <antoniovrdacosta@gmail.com>
2023-03-30 22:06:56 -0300

    feat: Add custom exceptions

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,7 +1,10 @@
 import { useState } from 'react'
+import { Alert } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
 
 import * as Styled from './styles'
+
+import { AppError } from '@utils/AppError'
 
 import { Button } from '@components/Button'
 import { Header } from '@components/Header'
@@ -15,8 +18,21 @@ export function NewGroup() {
   const navigation = useNavigation()
 
   async function handleNew() {
-    await groupCreate(group)
-    navigation.navigate('players', { group })
+    try {
+      if (group.trim().length === 0) {
+        return Alert.alert('Novo Grupo', 'Informe o nome da turma.')
+      }
+
+      await groupCreate(group)
+      navigation.navigate('players', { group })
+    } catch (error) {
+      if (error instanceof AppError) {
+        Alert.alert('Novo Grupo', error.message)
+      } else {
+        Alert.alert('Novo Grupo', 'Não foi possível criar um novo grupo.')
+        console.error(error)
+      }
+    }
   }
 
   return (
